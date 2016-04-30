2792eed6ac581db5a9490462a9c905d94097cbb9
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:11:47 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/BackgroundColor.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/BackgroundColor.java
@@ -304,12 +304,11 @@ public class BackgroundColor extends AbstractCssProperty<BackgroundColor>
             if (!trimmedCssValue.startsWith("#")) {
                 return false;
             }
+
             final long value = Long.parseLong(cssValue.replaceFirst("[#]", ""),
                     16);
-            if (value > CommonConstants.FFFFFF_HEX_VALUE || value < 0) {
-                return false;
-            }
-            return true;
+
+            return !(value > CommonConstants.FFFFFF_HEX_VALUE || value < 0);
         } catch (final NumberFormatException ex) {
         }
 
