dd2db8a195accede2bd56fcd3ba329da3ec8199d
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:58:05 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/AnimationIterationCount.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/AnimationIterationCount.java
@@ -248,11 +248,9 @@ public class AnimationIterationCount
         }
         try {
             final int parsedValue = Integer.parseInt(trimmedCssValue);
-            if (parsedValue == 0 && (trimmedCssValue.contains("-")
-                    || trimmedCssValue.contains("+"))) {
-                return false;
-            }
-            return true;
+
+            return !(parsedValue == 0 && (trimmedCssValue.contains("-")
+                    || trimmedCssValue.contains("+")));
         } catch (final NumberFormatException e) {
         }
 
