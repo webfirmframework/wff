322548d8cd0a8de3ca2b538f0a56c8d74766e60d
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:21:41 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/FlexBasis.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/FlexBasis.java
@@ -298,11 +298,14 @@ public class FlexBasis extends AbstractCssProperty<FlexBasis> {
      * @since 1.0.0
      */
     public static boolean isValid(final String cssValue) {
+
         final String trimmedCssValue = TagStringUtil
                 .toLowerCase(cssValue.trim());
+
         if (trimmedCssValue.contains(" ")) {
             return false;
         }
+
         for (final CssLengthUnit cssLengthUnit : CssLengthUnit.values()) {
             final String unit = cssLengthUnit.getUnit();
             if (trimmedCssValue.endsWith(unit)) {
@@ -315,9 +318,7 @@ public class FlexBasis extends AbstractCssProperty<FlexBasis> {
                 return true;
             }
         }
-        if (PREDEFINED_CONSTANTS.contains(trimmedCssValue)) {
-            return true;
-        }
-        return false;
+
+        return PREDEFINED_CONSTANTS.contains(trimmedCssValue);
     }
 }
