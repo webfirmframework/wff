557e5edca4e309ec33bc82eb417aff72334bac7c
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:23:13 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/BorderBottomColor.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/BorderBottomColor.java
@@ -299,15 +299,19 @@ public class BorderBottomColor extends AbstractCssProperty<BorderBottomColor>
         if (CssColorName.isValid(trimmedCssValue)) {
             return true;
         }
+
         if (RgbCssValue.isValid(trimmedCssValue)) {
             return true;
         }
+
         if (RgbaCssValue.isValid(trimmedCssValue)) {
             return true;
         }
+
         if (HslCssValue.isValid(trimmedCssValue)) {
             return true;
         }
+
         if (HslaCssValue.isValid(trimmedCssValue)) {
             return true;
         }
@@ -316,12 +320,11 @@ public class BorderBottomColor extends AbstractCssProperty<BorderBottomColor>
             if (!trimmedCssValue.startsWith("#")) {
                 return false;
             }
+
             final long value = Long.parseLong(cssValue.replaceFirst("[#]", ""),
                     16);
-            if (value > CommonConstants.FFFFFF_HEX_VALUE || value < 0) {
-                return false;
-            }
-            return true;
+
+            return !(value > CommonConstants.FFFFFF_HEX_VALUE || value < 0);
         } catch (final NumberFormatException ex) {
         }
 
