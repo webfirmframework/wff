e83d29504b0f7257f3f6bc426b414646ea2e342b
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 16:15:48 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnRuleColor.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnRuleColor.java
@@ -219,10 +219,8 @@ public class MozColumnRuleColor
             }
             final long value = Long.parseLong(cssValue.replaceFirst("[#]", ""),
                     16);
-            if (value > CommonConstants.FFFFFF_HEX_VALUE || value < 0) {
-                return false;
-            }
-            return true;
+
+            return !(value > CommonConstants.FFFFFF_HEX_VALUE || value < 0);
         } catch (final NumberFormatException ex) {
         }
 
