3d493e3a4673ba0377a8da3bfd025079a467a540
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:44:02 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/MarginBottom.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/MarginBottom.java
@@ -297,10 +297,13 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
      * @since 1.0.0
      */
     public static boolean isValid(final String cssValue) {
+
         final String trimmedCssValue = cssValue.trim().toLowerCase();
+
         if (trimmedCssValue.contains(" ")) {
             return false;
         }
+
         for (final CssLengthUnit cssLengthUnit : CssLengthUnit.values()) {
             final String unit = cssLengthUnit.getUnit();
             if (trimmedCssValue.endsWith(unit)) {
@@ -315,9 +318,7 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
                 return true;
             }
         }
-        if (PREDEFINED_CONSTANTS.contains(trimmedCssValue)) {
-            return true;
-        }
-        return false;
+
+        return PREDEFINED_CONSTANTS.contains(trimmedCssValue);
     }
 }
