fd829b5f75e596ce1c3d9da4695cd40e4bc61b90
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:50:35 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnCount.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnCount.java
@@ -242,12 +242,11 @@ public class MozColumnCount extends AbstractCssProperty<MozColumnCount> {
         }
         try {
             final int parsedValue = Integer.parseInt(trimmedCssValue);
-            if (parsedValue == 0 && (trimmedCssValue.contains("-")
-                    || trimmedCssValue.contains("+"))) {
-                return false;
-            }
-            return true;
+
+            return !(parsedValue == 0 && (trimmedCssValue.contains("-")
+                    || trimmedCssValue.contains("+")));
         } catch (final NumberFormatException e) {
+            // NOP
         }
 
         return PREDEFINED_CONSTANTS.contains(trimmedCssValue);
