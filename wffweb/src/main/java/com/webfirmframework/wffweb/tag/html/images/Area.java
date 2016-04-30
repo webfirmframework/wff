db204d4575cbd7fe8d101fe1a21d0c233bc03b6a
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 16:48:40 +0530

    Code formatting improvement
    Added needful comment in empty method body

--- wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/images/Area.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/images/Area.java
@@ -58,6 +58,7 @@ public class Area extends AbstractHtml implements AreaAttributable {
      * @since 1.0.0
      */
     protected void init() {
+        // to override and use this method
     }
 
 }
