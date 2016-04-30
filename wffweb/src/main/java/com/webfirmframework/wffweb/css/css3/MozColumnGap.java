ff960b16c37dbf19e61512cc8981bd66f10c96c4
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 15:59:34 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnGap.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/MozColumnGap.java
@@ -293,10 +293,13 @@ public class MozColumnGap extends AbstractCssProperty<MozColumnGap> {
      * @since 1.0.0
      */
     public static boolean isValid(final String cssValue) {
+
         final String trimmedCssValue = cssValue.trim().toLowerCase();
+
         if (trimmedCssValue.contains(" ")) {
             return false;
         }
+
         for (final CssLengthUnit cssLengthUnit : CssLengthUnit.values()) {
             final String unit = cssLengthUnit.getUnit();
             if (trimmedCssValue.endsWith(unit)) {
@@ -309,9 +312,7 @@ public class MozColumnGap extends AbstractCssProperty<MozColumnGap> {
                 return true;
             }
         }
-        if (PREDEFINED_CONSTANTS.contains(trimmedCssValue)) {
-            return true;
-        }
-        return false;
+
+        return PREDEFINED_CONSTANTS.contains(trimmedCssValue);
     }
 }
