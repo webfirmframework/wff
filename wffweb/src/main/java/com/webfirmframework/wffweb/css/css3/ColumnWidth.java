1529c24ed34876e2e7f95e206fb91138ba7c1a25
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 16:14:41 +0530

    Code improvement
    Avoided unnecessary if..then..else statements when returning booleans

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/ColumnWidth.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/css3/ColumnWidth.java
@@ -299,11 +299,14 @@ public class ColumnWidth extends AbstractCssProperty<ColumnWidth> {
      * @since 1.0.0
      */
     public static boolean isValid(final String cssValue) {
+
         final String trimmedCssValue = TagStringUtil
                 .toLowerCase(cssValue.trim());
+
         if (trimmedCssValue.contains(" ")) {
             return false;
         }
+
         for (final CssLengthUnit cssLengthUnit : CssLengthUnit.values()) {
             final String unit = cssLengthUnit.getUnit();
             if (trimmedCssValue.endsWith(unit)) {
@@ -316,9 +319,7 @@ public class ColumnWidth extends AbstractCssProperty<ColumnWidth> {
                 return true;
             }
         }
-        if (PREDEFINED_CONSTANTS.contains(trimmedCssValue)) {
-            return true;
-        }
-        return false;
+
+        return PREDEFINED_CONSTANTS.contains(trimmedCssValue);
     }
 }
