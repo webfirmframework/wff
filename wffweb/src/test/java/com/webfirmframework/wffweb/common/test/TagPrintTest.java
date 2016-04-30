310a0afbf29697bc0648cb1b7eeeba60d1caa224
Web Firm Framework <webfirm.framework@gmail.com>
2016-04-30 11:55:09 +0530

    Code improvement
    Perhaps 'afterTimeMillis' could be replaced by a local variable.

--- wffweb/src/test/java/com/webfirmframework/wffweb/common/test/TagPrintTest.java
+++ wffweb/src/test/java/com/webfirmframework/wffweb/common/test/TagPrintTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014-2016 Web Firm Framework
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,20 +42,18 @@ import com.webfirmframework.wffweb.tag.htmlwff.CustomTag;
 import com.webfirmframework.wffweb.view.AbstractHtmlView;
 
 /**
- * 
+ *
  * @author WFF
  * @since 1.0.0
  */
 public class TagPrintTest implements Serializable {
 
     private static final long serialVersionUID = 1_0_0L;
-    
+
     private long beforeTimeMillis;
 
-    private long afterTimeMillis;
-
     /**
-     * 
+     *
      * @author WFF
      * @since 1.0.0
      */
@@ -65,35 +63,55 @@ public class TagPrintTest implements Serializable {
     }
 
     /**
-     * 
+     *
      * @author WFF
      * @since 1.0.0
      */
     @After
     public void after() {
-        afterTimeMillis = System.currentTimeMillis();
+
+        final long afterTimeMillis = System.currentTimeMillis();
         System.out.println("beforeTimeMillis " + beforeTimeMillis);
         System.out.println("afterTimeMillis " + afterTimeMillis);
         System.out.println(afterTimeMillis - beforeTimeMillis);
-        
+
     }
 
     @Test
     public void testCloneTag() throws Exception {
         final Width width1 = new Width(1);
         final Html html1 = new Html(1, null, width1) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             Html html2 = new Html(2, this, new Width(2)) {
 
+                /**
+                 *
+                 */
+                private static final long serialVersionUID = 1L;
+
                 Html html3 = new Html(3, this, new Width(3)) {
+
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
                 };
 
                 Html html4 = new Html(4, this, new Width(4)) {
+
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
                 };
 
             };
 
         };
-        Html clonedHtml1 = html1.clone();
+        final Html clonedHtml1 = html1.clone();
         Assert.assertNotEquals(clonedHtml1, html1);
 
     }
@@ -102,19 +120,38 @@ public class TagPrintTest implements Serializable {
     public void testToStringPrint() throws Exception {
         final Width width1 = new Width(1);
         final Html html1 = new Html(1, null, width1, new Id("html1Id")) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             Html html2 = new Html(2, this, new Width(2)) {
 
+                /**
+                 *
+                 */
+                private static final long serialVersionUID = 1L;
+
                 Html html3 = new Html(3, this, new Width(3)) {
+
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
                 };
 
                 Html html4 = new Html(4, this, new Width(4)) {
+
+                    /**
+                     *
+                     */
+                    private static final long serialVersionUID = 1L;
                 };
 
             };
 
         };
-        String expected = "<!DOCTYPE html>\n<html1 width=\"1.0%\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
-//         System.out.println(html1);
+        final String expected = "<!DOCTYPE html>\n<html1 width=\"1.0%\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
+        // System.out.println(html1);
         // System.out.println();
         // System.out.println(expected);
         html1.setPrependDocType(true);
@@ -122,23 +159,23 @@ public class TagPrintTest implements Serializable {
 
         {
             width1.setValue(56, CssLengthUnit.PER);
-            String expected2 = "<!DOCTYPE html>\n<html1 width=\"56%\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
+            final String expected2 = "<!DOCTYPE html>\n<html1 width=\"56%\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
             Assert.assertEquals(expected2, html1.toString());
         }
 
         {
             width1.setValue(55, CssLengthUnit.PX);
-            String expected2 = "<!DOCTYPE html>\n<html1 width=\"55px\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
+            final String expected2 = "<!DOCTYPE html>\n<html1 width=\"55px\" id=\"html1Id\"><html2 width=\"2.0%\"><html3 width=\"3.0%\"></html3><html4 width=\"4.0%\"></html4></html2></html1>";
             Assert.assertEquals(expected2, html1.toString());
         }
 
     }
-    
+
     @Test
     public void testMultipleAttributesToHtmlString() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
-        String expectedString = "<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
+        final String expectedString = "<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
         Assert.assertEquals(expectedString, html.toString());
 
         if (html.toHtmlString().contains("\0")) {
@@ -148,136 +185,186 @@ public class TagPrintTest implements Serializable {
 
     @Test
     public void testSingleHtmlTag() throws Exception {
-        Html html = new Html(null);
-        String expectedString = "<html></html>";
+        final Html html = new Html(null);
+        final String expectedString = "<html></html>";
         Assert.assertEquals(expectedString, html.toString());
     }
-    
+
     @SuppressWarnings("serial")
     private class SampleAbstractHtmlView extends AbstractHtmlView {
 
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+
         @Override
-        public void develop(OutputBuffer out) {
-            Html html = new Html(null);
+        public void develop(final OutputBuffer out) {
+            final Html html = new Html(null);
             out.append(html);
         }
-        
+
     }
-    
+
     @Test
     public void testSingleHtmlTag2() throws Exception {
-        SampleAbstractHtmlView view = new SampleAbstractHtmlView();
-        String expectedString = "<html></html>";
+        final SampleAbstractHtmlView view = new SampleAbstractHtmlView();
+        final String expectedString = "<html></html>";
         Assert.assertEquals(expectedString, view.toString());
         Assert.assertEquals(expectedString, view.toString());
     }
-    
+
     @Test(expected = AssertionError.class)
     public void testSingleHtmlTag3() throws Exception {
-        SampleAbstractHtmlView view = new SampleAbstractHtmlView();
+        final SampleAbstractHtmlView view = new SampleAbstractHtmlView();
         view.setPreserveOutputBufferContent(true);
-        String expectedString = "<html></html>";
+        final String expectedString = "<html></html>";
         Assert.assertEquals(expectedString, view.toString());
         Assert.assertEquals(expectedString, view.toString());
     }
-    
+
     @Test
     public void testCustomTag1() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.OPENING_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.OPENING_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.OPENING_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag></anothertag></newtag>";
+        final String expectedString = "<newtag><anothertag></anothertag></newtag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
-    
+
     @Test
     public void testCustomTag2() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.OPENING_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.OPENING_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.SELF_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag/></newtag>";
+        final String expectedString = "<newtag><anothertag/></newtag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
 
     @Test
     public void testCustomTag3() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.OPENING_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.OPENING_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.NON_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag></newtag>";
+        final String expectedString = "<newtag><anothertag></newtag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
-    
+
     @Test
     public void testCustomTag4() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING,
+                null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.OPENING_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag></anothertag>";
+        final String expectedString = "<newtag><anothertag></anothertag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
-    
+
     @Test
     public void testCustomTag5() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING,
+                null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.SELF_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag/>";
+        final String expectedString = "<newtag><anothertag/>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
 
     @Test
     public void testCustomTag6() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag", TagType.NON_CLOSING,
+                null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.NON_CLOSING,
                     this);
         };
-        String expectedString = "<newtag><anothertag>";
+        final String expectedString = "<newtag><anothertag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
+
     @Test
     public void testCustomTag7() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.SELF_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.SELF_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.OPENING_CLOSING,
                     this);
         };
-        String expectedString = "<newtag/><anothertag></anothertag>";
+        final String expectedString = "<newtag/><anothertag></anothertag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
-    
+
     @Test
     public void testCustomTag8() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.SELF_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.SELF_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.SELF_CLOSING,
                     this);
         };
-        String expectedString = "<newtag/><anothertag/>";
+        final String expectedString = "<newtag/><anothertag/>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
 
     @Test
     public void testCustomTag9() throws Exception {
-        CustomTag customTag = new CustomTag("newtag", TagType.SELF_CLOSING, null) {
+        final CustomTag customTag = new CustomTag("newtag",
+                TagType.SELF_CLOSING, null) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             CustomTag c = new CustomTag("anothertag", TagType.NON_CLOSING,
                     this);
         };
-        String expectedString = "<newtag/><anothertag>";
+        final String expectedString = "<newtag/><anothertag>";
         Assert.assertEquals(expectedString, customTag.toHtmlString());
     }
-    
+
     @Test
     public void testMetaInfoTags() throws Exception {
-        Div div = new Div(null, new Id("1")) {
+        final Div div = new Div(null, new Id("1")) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
             Br br = new Br(this);
             Link link = new Link(this);
             Meta meta = new Meta(this);
@@ -286,54 +373,58 @@ public class TagPrintTest implements Serializable {
             Base base = new Base(null);
 
         };
-        String expectedString = "<div id=\"1\"><br/><link/><meta/><div id=\"2\"></div></div>";
+        final String expectedString = "<div id=\"1\"><br/><link/><meta/><div id=\"2\"></div></div>";
         Assert.assertEquals(expectedString, div.toHtmlString());
     }
-    
+
     @Test
     public void testToHtmlString() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
         html.setPrependDocType(true);
-        String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        final String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
         Assert.assertEquals(expectedString, html.toHtmlString());
 
     }
-    
+
     @Test
     public void testToHtmlStringWithCharset1() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
         html.setPrependDocType(true);
-        String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
-        Assert.assertEquals(expectedString, html.toHtmlString(StandardCharsets.UTF_8));
+        final String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        Assert.assertEquals(expectedString,
+                html.toHtmlString(StandardCharsets.UTF_8));
     }
-    
+
     @Test
     public void testToHtmlStringWithCharset2() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
         html.setPrependDocType(true);
-        String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
-        Assert.assertEquals(expectedString, html.toHtmlString(StandardCharsets.UTF_8.name()));
+        final String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        Assert.assertEquals(expectedString,
+                html.toHtmlString(StandardCharsets.UTF_8.name()));
     }
-    
+
     @Test
     public void testToHtmlStringWithCharset3() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
         html.setPrependDocType(true);
-        String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
-        Assert.assertEquals(expectedString, html.toHtmlString(true, StandardCharsets.UTF_8));
+        final String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        Assert.assertEquals(expectedString,
+                html.toHtmlString(true, StandardCharsets.UTF_8));
     }
-    
+
     @Test
     public void testToHtmlStringWithCharset4() throws Exception {
-        Html html = new Html(null, new Hidden(), new Id("htmlId"), new Style(
-                "color:red;width:15px"));
+        final Html html = new Html(null, new Hidden(), new Id("htmlId"),
+                new Style("color:red;width:15px"));
         html.setPrependDocType(true);
-        String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
-        Assert.assertEquals(expectedString, html.toHtmlString(true, StandardCharsets.UTF_8.name()));
+        final String expectedString = "<!DOCTYPE html>\n<html hidden id=\"htmlId\" style=\"color:red;width:15px;\"></html>";
+        Assert.assertEquals(expectedString,
+                html.toHtmlString(true, StandardCharsets.UTF_8.name()));
     }
 
 }
