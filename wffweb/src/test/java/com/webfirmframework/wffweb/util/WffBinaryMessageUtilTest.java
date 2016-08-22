e7c9f6314a4235c23611a2a0d425ee378826fd20
Web Firm Framework <webfirm.framework@gmail.com>
2016-08-22 21:32:46 +0530

    Minor improvement

--- wffweb/src/test/java/com/webfirmframework/wffweb/util/WffBinaryMessageUtilTest.java
+++ wffweb/src/test/java/com/webfirmframework/wffweb/util/WffBinaryMessageUtilTest.java
@@ -466,7 +466,7 @@ public class WffBinaryMessageUtilTest {
         
         long before = System.currentTimeMillis();
         
-        for (int i = 0; i <= 35566 * 2; i++) {
+        for (int i = 0; i <= 35566 * 4; i++) {
             @SuppressWarnings("unused")
             int len = WffBinaryMessageUtil.getOptimizedBytesFromInt(i).length;
         }
@@ -478,7 +478,7 @@ public class WffBinaryMessageUtilTest {
         System.out.println("time taken for getOptimizedBytesFromInt "+difference1);
         
         before = System.currentTimeMillis();
-        for (int i = 0; i <= 35566 * 2; i++) {
+        for (int i = 0; i <= 35566 * 4; i++) {
           @SuppressWarnings("unused")
           int len = WffBinaryMessageUtil.getLengthOfOptimizedBytesFromInt(i);
         }
