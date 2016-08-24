9754d360ac47d426b218cf65f1e8b9f43236edc4
Web Firm Framework <webfirm.framework@gmail.com>
2016-08-24 11:52:04 +0530

    Implemented AbstractHtml#clone(String... excludeAttributes)
    This method will return a cloned copy of the object excluding the given
    attributes (which are as attributes names) in all tags including
    recursing children.

--- wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/AbstractHtml.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/AbstractHtml.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import com.webfirmframework.wffweb.clone.CloneUtil;
 import com.webfirmframework.wffweb.streamer.WffBinaryMessageOutputStreamer;
@@ -1070,6 +1071,100 @@ public abstract class AbstractHtml extends AbstractTagBase {
     @Override
     public AbstractHtml clone() throws CloneNotSupportedException {
         return deepClone(this);
+    }
+
+    /**
+     * Creates and returns a deeply cloned copy of this object. The precise
+     * meaning of "copy" may depend on the class of the object. The general
+     * intent is that, for any object {@code x}, the expression: <blockquote>
+     *
+     * <pre>
+     * x.clone() != x
+     * </pre>
+     *
+     * </blockquote> will be true, and that the expression: <blockquote>
+     *
+     * <pre>
+     * x.clone().getClass() == x.getClass()
+     * </pre>
+     *
+     * </blockquote> will be {@code true}, but these are not absolute
+     * requirements. While it is typically the case that: <blockquote>
+     *
+     * <pre>
+     * x.clone().equals(x)
+     * </pre>
+     *
+     * </blockquote> will be {@code true}, this is not an absolute requirement.
+     * <p>
+     * By convention, the returned object should be obtained by calling
+     * {@code super.clone}. If a class and all of its superclasses (except
+     * {@code Object}) obey this convention, it will be the case that
+     * {@code x.clone().getClass() == x.getClass()}.
+     * <p>
+     * By convention, the object returned by this method should be independent
+     * of this object (which is being cloned). To achieve this independence, it
+     * may be necessary to modify one or more fields of the object returned by
+     * {@code super.clone} before returning it. Typically, this means copying
+     * any mutable objects that comprise the internal "deep structure" of the
+     * object being cloned and replacing the references to these objects with
+     * references to the copies. If a class contains only primitive fields or
+     * references to immutable objects, then it is usually the case that no
+     * fields in the object returned by {@code super.clone} need to be modified.
+     * <p>
+     * The method {@code clone} for class {@code AbstractHtml} performs a
+     * specific cloning operation. First, if the class of this object does not
+     * implement the interfaces {@code Cloneable} and {@code Serializable}, then
+     * a {@code CloneNotSupportedException} is thrown. Note that all arrays are
+     * considered to implement the interface {@code Cloneable} and that the
+     * return type of the {@code clone} method of an array type {@code T[]} is
+     * {@code T[]} where T is any reference or primitive type. Otherwise, this
+     * method creates a new instance of the class of this object and initializes
+     * all its fields with exactly the contents of the corresponding fields of
+     * this object, as if by assignment; the contents of the fields are not
+     * themselves cloned. Thus, this method performs a "shallow copy" of this
+     * object, not a "deep copy" operation.
+     *
+     * @param excludeAttributes
+     *            pass the attributes names which need to be excluded from all
+     *            tags including their child tags.
+     *
+     * @return a deep clone of this instance without the given attributes.
+     * @exception CloneNotSupportedException
+     *                if the object's class does not support the
+     *                {@code Cloneable} and {@code Serializable} interfaces.
+     *                Subclasses that override the {@code clone} method can also
+     *                throw this exception to indicate that an instance cannot
+     *                be cloned.
+     * @see java.lang.Cloneable
+     * @see java.io.Serializable
+     * @author WFF
+     * @since 1.2.0
+     */
+    public AbstractHtml clone(final String... excludeAttributes)
+            throws CloneNotSupportedException {
+
+        final AbstractHtml clonedObject = deepClone(this);
+
+        final Stack<List<AbstractHtml>> childrenStack = new Stack<List<AbstractHtml>>();
+        childrenStack.push(Arrays.asList(clonedObject));
+
+        while (childrenStack.size() > 0) {
+
+            final List<AbstractHtml> children = childrenStack.pop();
+
+            for (final AbstractHtml child : children) {
+                child.removeAttributes(excludeAttributes);
+
+                final List<AbstractHtml> subChildren = child.getChildren();
+                if (subChildren != null && subChildren.size() > 0) {
+                    childrenStack.push(subChildren);
+                }
+            }
+
+        }
+
+        return clonedObject;
     }
 
     /**
