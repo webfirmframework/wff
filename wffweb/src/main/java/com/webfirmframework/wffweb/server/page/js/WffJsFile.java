c43cb12c32d8843a1b184af713b29e50b689b0e6
Web Firm Framework <webfirm.framework@gmail.com>
2016-12-19 08:48:32 +0530

    Minor optimization and security improvements
    The generated JavaScript code is now minified to short variable and
    method names.

--- wffweb/src/main/java/com/webfirmframework/wffweb/server/page/js/WffJsFile.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/server/page/js/WffJsFile.java
@@ -18,7 +18,7 @@ package com.webfirmframework.wffweb.server.page.js;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -68,51 +68,124 @@ public enum WffJsFile {
 
     private String optimizedFileContent;
 
-    private static final Set<String> FUNCTION_NAMES = new HashSet<String>();
+    private static Set<String> FUNCTION_NAMES;
+
+    private static Set<String> VARIABLE_NAMES;
 
     private static volatile int functionId = 0;
+
+    private static volatile int variableId = 0;
 
     static {
 
         if (PRODUCTION_MODE) {
 
-            FUNCTION_NAMES.add("getWffBinaryMessageBytes");
-            FUNCTION_NAMES.add("getTagByTagNameAndWffId");
-            FUNCTION_NAMES.add("getTagByWffId");
-            FUNCTION_NAMES.add("getWffIdFromWffIdBytes");
-            FUNCTION_NAMES.add("getWffIdBytesFromTag");
-            FUNCTION_NAMES.add("splitAttrNameValue");
-            FUNCTION_NAMES.add("createTagFromWffBMBytes");
-            FUNCTION_NAMES.add("getTagDeletedWffBMBytes");
-            FUNCTION_NAMES.add("getAttrUpdatedWffBMBytes");
-            FUNCTION_NAMES.add("recurChild");
-            FUNCTION_NAMES.add("getTagCreatedWffBMBytes");
-            FUNCTION_NAMES.add("getWffBinaryMessageBytes");
-            FUNCTION_NAMES.add("parseWffBinaryMessageBytes");
-            FUNCTION_NAMES.add("extractValuesFromValueBytes");
-            FUNCTION_NAMES.add("concatArrayValues");
+            FUNCTION_NAMES = new LinkedHashSet<String>();
+            VARIABLE_NAMES = new LinkedHashSet<String>();
+
+            // should be in descending order of the value length
+
+            FUNCTION_NAMES.add("getLengthOfOptimizedBytesFromInt");
             FUNCTION_NAMES.add("concatArrayValuesFromPosition");
-            FUNCTION_NAMES.add("getIntFromBytes");
-            FUNCTION_NAMES.add("getBytesFromInt");
+            FUNCTION_NAMES.add("extractValuesFromValueBytes");
+            FUNCTION_NAMES.add("getDoubleFromOptimizedBytes");
+            FUNCTION_NAMES.add("parseWffBinaryMessageBytes");
+            FUNCTION_NAMES.add("getAttrUpdatedWffBMBytes");
+            FUNCTION_NAMES.add("getWffBinaryMessageBytes");
+            FUNCTION_NAMES.add("getWffBinaryMessageBytes");
             FUNCTION_NAMES.add("getIntFromOptimizedBytes");
             FUNCTION_NAMES.add("getOptimizedBytesFromInt");
-            FUNCTION_NAMES.add("getLengthOfOptimizedBytesFromInt");
-            FUNCTION_NAMES.add("getLastBytesFromInt");
-            FUNCTION_NAMES.add("getBytesFromDouble");
-            FUNCTION_NAMES.add("getDoubleFromOptimizedBytes");
-            FUNCTION_NAMES.add("getValueTypeByte");
-            FUNCTION_NAMES.add("invokeTask");
-            FUNCTION_NAMES.add("invokeTasks");
-            FUNCTION_NAMES.add("getAttributeUpdates");
-            FUNCTION_NAMES.add("getStringFromBytes");
-            FUNCTION_NAMES.add("getTaskNameValue");
-            FUNCTION_NAMES.add("getStringFromBytes");
             FUNCTION_NAMES.add("getTagByTagNameAndWffId");
-            FUNCTION_NAMES.add("getTagByWffId");
+            FUNCTION_NAMES.add("getTagDeletedWffBMBytes");
+            FUNCTION_NAMES.add("createTagFromWffBMBytes");
+            FUNCTION_NAMES.add("getTagCreatedWffBMBytes");
+            FUNCTION_NAMES.add("getTagByTagNameAndWffId");
+            FUNCTION_NAMES.add("createTagFromWffBMBytes");
+            FUNCTION_NAMES.add("getWffIdFromWffIdBytes");
             FUNCTION_NAMES.add("getWffIdFromWffIdBytes");
             FUNCTION_NAMES.add("getWffIdBytesFromTag");
+            FUNCTION_NAMES.add("getWffIdBytesFromTag");
+            FUNCTION_NAMES.add("getLastBytesFromInt");
+            FUNCTION_NAMES.add("getAttributeUpdates");
+            FUNCTION_NAMES.add("getStringFromBytes");
             FUNCTION_NAMES.add("splitAttrNameValue");
-            FUNCTION_NAMES.add("createTagFromWffBMBytes");
+            FUNCTION_NAMES.add("getBytesFromDouble");
+            FUNCTION_NAMES.add("getStringFromBytes");
+            FUNCTION_NAMES.add("splitAttrNameValue");
+            FUNCTION_NAMES.add("concatArrayValues");
+            FUNCTION_NAMES.add("getTaskNameValue");
+            FUNCTION_NAMES.add("getValueTypeByte");
+            FUNCTION_NAMES.add("getIntFromBytes");
+            FUNCTION_NAMES.add("getBytesFromInt");
+            FUNCTION_NAMES.add("getTagByWffId");
+            FUNCTION_NAMES.add("getTagByWffId");
+            FUNCTION_NAMES.add("invokeTasks");
+            FUNCTION_NAMES.add("recurChild");
+            FUNCTION_NAMES.add("invokeTask");
+
+            VARIABLE_NAMES.add("maxBytesLengthFromTotalBytes");
+            VARIABLE_NAMES.add("maxBytesLengthForAllValues");
+            VARIABLE_NAMES.add("indexInWffBinaryMessage");
+            VARIABLE_NAMES.add("valueLengthBytesLength");
+            VARIABLE_NAMES.add("wffBinaryMessageBytes");
+            VARIABLE_NAMES.add("maxNoValueLengthBytes");
+            VARIABLE_NAMES.add("attrNameAndValueBytes");
+            VARIABLE_NAMES.add("nameLengthBytesLength");
+            VARIABLE_NAMES.add("extractEachValueBytes");
+            VARIABLE_NAMES.add("superParentNameValue");
+            VARIABLE_NAMES.add("currentParentTagName");
+            VARIABLE_NAMES.add("maxNoNameLengthBytes");
+            VARIABLE_NAMES.add("attrNameValueBytes");
+            VARIABLE_NAMES.add("currentParentWffId");
+            VARIABLE_NAMES.add("maxNoOfValuesBytes");
+            VARIABLE_NAMES.add("attrNameValueArry");
+            VARIABLE_NAMES.add("superParentValues");
+            VARIABLE_NAMES.add("indexOfSeparator");
+            VARIABLE_NAMES.add("argumentBMObject");
+            VARIABLE_NAMES.add("currentParentTag");
+            VARIABLE_NAMES.add("maxNoOfNameBytes");
+            VARIABLE_NAMES.add("valueLengthBytes");
+            VARIABLE_NAMES.add("valueLegthBytes");
+            VARIABLE_NAMES.add("valuesToAppend");
+            VARIABLE_NAMES.add("beforeTagWffId");
+            VARIABLE_NAMES.add("nodeValueBytes");
+            VARIABLE_NAMES.add("fromByteArray");
+            VARIABLE_NAMES.add("applicableTag");
+            VARIABLE_NAMES.add("beforeTagName");
+            VARIABLE_NAMES.add("sOrCUtf8Bytes");
+            VARIABLE_NAMES.add("appendToArray");
+            VARIABLE_NAMES.add("parentTagName");
+            VARIABLE_NAMES.add("nodeNameBytes");
+            VARIABLE_NAMES.add("attrNameValue");
+            VARIABLE_NAMES.add("childTagName");
+            VARIABLE_NAMES.add("parentIndex");
+            VARIABLE_NAMES.add("tagToRemove");
+            VARIABLE_NAMES.add("zerothIndex");
+            VARIABLE_NAMES.add("secondIndex");
+            VARIABLE_NAMES.add("thirdIndex");
+            VARIABLE_NAMES.add("valueLegth");
+            VARIABLE_NAMES.add("childWffId");
+            VARIABLE_NAMES.add("intIdBytes");
+            VARIABLE_NAMES.add("wffIdBytes");
+            VARIABLE_NAMES.add("nameValues");
+            VARIABLE_NAMES.add("valueBytes");
+            VARIABLE_NAMES.add("valuesFrom");
+            VARIABLE_NAMES.add("firstIndex");
+            VARIABLE_NAMES.add("nameValue");
+            VARIABLE_NAMES.add("nameBytes");
+            VARIABLE_NAMES.add("beforeTag");
+            VARIABLE_NAMES.add("attrValue");
+            VARIABLE_NAMES.add("attrValue");
+            VARIABLE_NAMES.add("htmlNodes");
+            VARIABLE_NAMES.add("wffTagId");
+            VARIABLE_NAMES.add("attrName");
+            VARIABLE_NAMES.add("childTag");
+            VARIABLE_NAMES.add("allTags");
+            VARIABLE_NAMES.add("allTags");
+            VARIABLE_NAMES.add("wffIds");
+            VARIABLE_NAMES.add("wffId");
+            VARIABLE_NAMES.add("intId");
+            VARIABLE_NAMES.add("sOrC");
 
         }
     }
@@ -250,9 +323,17 @@ public enum WffJsFile {
 
             allOptimizedContent = builder.toString().trim();
 
-            for (final String name : FUNCTION_NAMES) {
-                allOptimizedContent = allOptimizedContent.replace(name,
-                        "f" + (++functionId));
+            if (PRODUCTION_MODE) {
+                for (final String name : FUNCTION_NAMES) {
+                    allOptimizedContent = allOptimizedContent.replace(name,
+                            "f" + (++functionId));
+                }
+
+                for (final String name : VARIABLE_NAMES) {
+                    allOptimizedContent = allOptimizedContent.replace(name,
+                            "v" + (++variableId));
+                }
+
             }
 
             return JS_WORK_AROUND.optimizedFileContent
