e81091de1857c63e0c16c2e4318f07f5d2070dc2
Web Firm Framework <webfirm.framework@gmail.com>
2017-01-13 18:03:13 +0530

    Minor improvement

--- wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/html5/attribute/DateTime.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/html5/attribute/DateTime.java
@@ -22,7 +22,7 @@ import com.webfirmframework.wffweb.tag.html.html5.identifier.TimeAttributable;
 
 /**
  *
- * <code>datetime</code> attribute for the time.<br/>
+ * <code>datetime</code> attribute for time tag.<br/>
  *
  * The content of the meta information.
  *
