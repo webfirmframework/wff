01cf7dd299f257a34e94fea950f98f6e6a6c43bf
Web Firm Framework <webfirm.framework@gmail.com>
2017-04-26 18:32:14 +0530

    Code optimization

--- wffweb/src/main/java/com/webfirmframework/wffweb/css/MarginBottom.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/css/MarginBottom.java
@@ -90,7 +90,7 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
     public MarginBottom(final float percent) {
         cssLengthUnit = CssLengthUnit.PER;
         value = percent;
-        cssValue = percent + "" + cssLengthUnit;
+        cssValue = String.valueOf(percent) + cssLengthUnit;
     }
 
     /**
@@ -100,7 +100,7 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
     public MarginBottom(final float value, final CssLengthUnit cssLengthUnit) {
         this.value = value;
         this.cssLengthUnit = cssLengthUnit;
-        cssValue = value + "" + cssLengthUnit;
+        cssValue = String.valueOf(value) + cssLengthUnit;
     }
 
     /**
@@ -114,7 +114,7 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
             final CssLengthUnit cssLengthUnit) {
         this.value = value;
         this.cssLengthUnit = cssLengthUnit;
-        cssValue = value + "" + cssLengthUnit;
+        cssValue = String.valueOf(value) + cssLengthUnit;
         if (getStateChangeInformer() != null) {
             getStateChangeInformer().stateChanged(this);
         }
@@ -130,7 +130,7 @@ public class MarginBottom extends AbstractCssProperty<MarginBottom> {
     public void setPercent(final float percent) {
         value = percent;
         cssLengthUnit = CssLengthUnit.PER;
-        cssValue = percent + "" + cssLengthUnit;
+        cssValue = String.valueOf(percent) + cssLengthUnit;
         if (getStateChangeInformer() != null) {
             getStateChangeInformer().stateChanged(this);
         }
