509182d6261137eb3c15854a3b5b175b36fa6346
Web Firm Framework <webfirm.framework@gmail.com>
2019-04-05 05:19:08 +0000

    Minor test case update for BackgroundImage class

--- wffweb/src/test/java/com/webfirmframework/wffweb/css/BackgroundImageTest.java
+++ wffweb/src/test/java/com/webfirmframework/wffweb/css/BackgroundImageTest.java
@@ -20,10 +20,13 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
 import com.webfirmframework.wffweb.InvalidValueException;
+import com.webfirmframework.wffweb.css.core.CssProperty;
+import com.webfirmframework.wffweb.css.file.CssBlock;
 
 /**
  * @author WFF
@@ -301,5 +304,45 @@ public class BackgroundImageTest {
 //    public void testStateChanged() {
 //        fail("Not yet implemented");
 //    }
+    
+    
+    private static final String URL_CSS_VALUE_STRING = "url(\"data:image/svg+xml;charset=utf8,<svg width='35px' height='35px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' class='uil-hourglass'><rect x='0' y='0' width='100' height='100' fill='none' class='bk'></rect><g><path fill='none' stroke='#007282' stroke-width='5' stroke-miterlimit='10' d='M58.4,51.7c-0.9-0.9-1.4-2-1.4-2.3s0.5-0.4,1.4-1.4 C70.8,43.8,79.8,30.5,80,15.5H70H30H20c0.2,15,9.2,28.1,21.6,32.3c0.9,0.9,1.4,1.2,1.4,1.5s-0.5,1.6-1.4,2.5 C29.2,56.1,20.2,69.5,20,85.5h10h40h10C79.8,69.5,70.8,55.9,58.4,51.7z' class='glass'></path><clipPath id='uil-hourglass-clip1'><rect x='15' y='20' width='70' height='25' class='clip'><animate attributeName='height' from='25' to='0' dur='1s' repeatCount='indefinite' vlaues='25;0;0' keyTimes='0;0.5;1'></animate><animate attributeName='y' from='20' to='45' dur='1s' repeatCount='indefinite' vlaues='20;45;45' keyTimes='0;0.5;1'></animate></rect></clipPath><clipPath id='uil-hourglass-clip2'><rect x='15' y='55' width='70' height='25' class='clip'><animate attributeName='height' from='0' to='25' dur='1s' repeatCount='indefinite' vlaues='0;25;25' keyTimes='0;0.5;1'></animate><animate attributeName='y' from='80' to='55' dur='1s' repeatCount='indefinite' vlaues='80;55;55' keyTimes='0;0.5;1'></animate></rect></clipPath><path d='M29,23c3.1,11.4,11.3,19.5,21,19.5S67.9,34.4,71,23H29z' clip-path='url(#uil-hourglass-clip1)' fill='#ffab00' class='sand'></path><path d='M71.6,78c-3-11.6-11.5-20-21.5-20s-18.5,8.4-21.5,20H71.6z' clip-path='url(#uil-hourglass-clip2)' fill='#ffab00' class='sand'></path><animateTransform attributeName='transform' type='rotate' from='0 50 50' to='180 50 50' repeatCount='indefinite' dur='1s' values='0 50 50;0 50 50;180 50 50' keyTimes='0;0.7;1'></animateTransform></g></svg>\")";
+    
+    @SuppressWarnings("serial")
+    private static final CssBlock processingSvg = new CssBlock(".processing") {
+
+        @Override
+        protected void load(final Set<CssProperty> cssProperties) {
+              UrlCss3Value urlCss3Value = new UrlCss3Value(URL_CSS_VALUE_STRING);
+              BackgroundImage backgroundImage = new BackgroundImage(urlCss3Value);
+            cssProperties.add(backgroundImage);
+            cssProperties.add(BackgroundRepeat.NO_REPEAT);
+        }
+        
+    };
+    
+    @Test
+    public void testDataStringInBackgroundImage() throws Exception {      
+        {
+            UrlCss3Value urlCss3Value = new UrlCss3Value(URL_CSS_VALUE_STRING);
+            
+            assertEquals(URL_CSS_VALUE_STRING, urlCss3Value.getValue());
+            assertEquals(URL_CSS_VALUE_STRING.substring(5, URL_CSS_VALUE_STRING.length() - 2), urlCss3Value.getUrl());
+            
+            System.out.println(urlCss3Value.toString());
+            
+            
+            
+            
+            BackgroundImage backgroundImage = new BackgroundImage(urlCss3Value);
+            
+            System.out.println(backgroundImage.toCssString());
+            
+            assertEquals(".processing{background-image:" + URL_CSS_VALUE_STRING + ";background-repeat:no-repeat;}", processingSvg.toCssString());
+            
+            System.out.println(processingSvg.toCssString());
+        }
+    }
+
 
 }
