929256c7cc18ac148dca05e13a4c59057eaa3f57
Web Firm Framework <webfirm.framework@gmail.com>
2021-10-05 16:17:50 +0530

    Minor improvements

--- wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/attribute/event/RemoteMethod.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/attribute/event/RemoteMethod.java
@@ -14,8 +14,13 @@ public interface RemoteMethod extends ServerAsyncMethod {
 
     WffBMObject orderedRun(final Event event);
 
+    /**
+     * @deprecated This method doesn't have any role in RemoteMethod implementation,
+     *             it's a dead method.
+     */
     @Override
+    @Deprecated
     default WffBMObject asyncMethod(final WffBMObject data, final Event event) {
-        return orderedRun(event);
+        return null;
     }
 }
