743906e06f8e27bbce704f3cb313c19ab7ff7e02
Web Firm Framework <webfirm.framework@gmail.com>
2022-01-20 12:59:20 +0530

    Client side changes for  AbstractHtml.whenURI method

--- wffweb/src/main/resources/com/webfirmframework/wffweb/internal/server/page/js/wffAsync.js
+++ wffweb/src/main/resources/com/webfirmframework/wffweb/internal/server/page/js/wffAsync.js
@@ -78,6 +78,100 @@ window.wffAsync = new function() {
 		return this;
 	};
 
+
+	var setServerURIWithCallback = function(uri, callback) {
+
+		var taskNameValue = wffTaskUtil.getTaskNameValue(
+			wffGlobal.taskValues.TASK,
+			wffGlobal.taskValues.CLIENT_PATHNAME_CHANGED);
+		var nameValue = {
+			'name': encoder.encode(uri),
+			'values': []
+		};
+		var nameValues = [taskNameValue, nameValue];
+
+		if (callback) {
+			var callbackFunId = wffAsync.generateUUID();
+			wffAsync.callbackFunctions[callbackFunId] = callback;
+
+			var nameValueCallbackFun = {
+				'name': encoder.encode(callbackFunId),
+				'values': []
+			};
+			nameValues.push(nameValueCallbackFun);
+		}
+
+		var wffBM = wffBMUtil.getWffBinaryMessageBytes(nameValues);
+		wffWS.send(wffBM);
+	};
+
+	this.setServerURI = function(uri) { setServerURIWithCallback(uri, undefined); };
+
+	var throwInvalidSetURIArgException = function() {
+		throw "Invalid argument found in setURI function call. " +
+		"Eg: wffAsync.setURI('/sampleuri', function(e){console.log('uri changed');}, function(e){console.log('uri changed, after server change');});, " +
+		"wffAsync.setURI('/sampleuri', function(e){console.log('uri changed');}); or " +
+		"wffAsync.setURI('/sampleuri');";
+	};
+
+	this.setURI = function(uri, onSetURI, afterSetURI) {
+
+		if (typeof onSetURI !== "undefined" && typeof onSetURI !== "function") {
+			throwInvalidSetURIArgException();
+		}
+
+		if (typeof afterSetURI !== "undefined" && typeof afterSetURI !== "function") {
+			throwInvalidSetURIArgException();
+		}
+
+		var uriBefore = window.location.pathname
+		history.pushState({}, document.title, uri);
+		var uriAfter = window.location.pathname
+		if (uriBefore !== uriAfter) {
+			var wffEvent = { uriBefore: uriBefore, uriAfter: uriAfter };
+
+			var callbackWrapper = afterSetURI;
+
+			if (typeof wffGlobalListeners !== "undefined") {
+				if (wffGlobalListeners.onSetURI) {
+					try {
+						wffGlobalListeners.onSetURI(wffEvent);
+					} catch (e) {
+						console.log("wffGlobalListeners.onSetURI threw exception when wffAsync.setURI is called", e);
+					}
+				}
+
+				if (wffGlobalListeners.afterSetURI) {
+					//NB: should be copied before using inside callbackWrapper
+					var afterSetURIGlobal = wffGlobalListeners.afterSetURI;
+					callbackWrapper = function() {
+						try {
+							afterSetURI(wffEvent);
+						} catch (e) {
+							console.log("The third argument threw exception when wffAsync.setURI is called", e);
+						}
+
+						try {
+							afterSetURIGlobal(wffEvent);
+						} catch (e) {
+							console.log("wffGlobalListeners.afterSetURI threw exception when wffAsync.setURI is called", e);
+						}
+
+					};
+				}
+
+			}
+
+			try {
+				onSetURI(wffEvent);
+			} catch (e) {
+				console.log("The second argument threw exception when wffAsync.setURI is called", e);
+			}
+			//NB: should be uriAfter to be consistent with uri pattern
+			setServerURIWithCallback(uriAfter, callbackWrapper);
+		}
+	};
+
 };
 
 // sample usage
