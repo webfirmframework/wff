993bff21245d3ed90e1264a1afc787c0080b92e6
Web Firm Framework <webfirm.framework@gmail.com>
2022-02-03 15:12:09 +0530

    Added preventDefault constructor param and setter-getter methods for it

--- wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/attribute/event/mouse/OnClick.java
+++ wffweb/src/main/java/com/webfirmframework/wffweb/tag/html/attribute/event/mouse/OnClick.java
@@ -54,8 +54,8 @@ public class OnClick extends AbstractEventAttribute implements AAttributable {
         setServerMethod(null, serverMethod, null, null);
     }
 
-    public OnClick(final String jsPreFunctionBody, final ServerMethod serverMethod,
-            final String jsFilterFunctionBody, final String jsPostFunctionBody) {
+    public OnClick(final String jsPreFunctionBody, final ServerMethod serverMethod, final String jsFilterFunctionBody,
+            final String jsPostFunctionBody) {
         setServerMethod(jsPreFunctionBody, serverMethod, jsFilterFunctionBody, jsPostFunctionBody);
     }
 
@@ -80,10 +80,204 @@ public class OnClick extends AbstractEventAttribute implements AAttributable {
      * @param serverSideData
      * @since 3.0.2
      */
-    public OnClick(final String jsPreFunctionBody, final ServerMethod serverMethod,
+    public OnClick(final String jsPreFunctionBody, final ServerMethod serverMethod, final String jsFilterFunctionBody,
+            final String jsPostFunctionBody, final Object serverSideData) {
+        setServerMethod(jsPreFunctionBody, serverMethod, jsFilterFunctionBody, jsPostFunctionBody, serverSideData);
+    }
+
+    /**
+     * @param preventDefault true to call event.preventDefault(); on event
+     * @param serverMethod   This method will invoke at server side with an argument
+     *                       {@code wffBMObject}. The {@code wffBMObject} is the
+     *                       representational JavaScript object returned by
+     *                       {@code jsFilterFunctionBody}.
+     * @since 12.0.0-beta.2
+     */
+    public OnClick(final boolean preventDefault, final ServerMethod serverMethod) {
+        setServerMethod(preventDefault, null, serverMethod, null, null, null);
+    }
+
+    /**
+     * @param preventDefault       true to call event.preventDefault(); on event
+     * @param jsPreFunctionBody    the body part JavaScript function (without
+     *                             function declaration). It must return true/false.
+     *                             This function will invoke at client side before
+     *                             {@code serverMethod}. If the jsPrefunction
+     *                             returns true then only {@code serverMethod}
+     *                             method will invoke (if it is implemented). It has
+     *                             implicit objects like {@code event} and
+     *                             {@code source} which gives the reference of the
+     *                             current tag. <br>
+     *                             Eg:-
+     *
+     *                             <pre>
+     *                                                                    if (source.type == 'button') {
+     *                                                                       return true;
+     *                                                                    }
+     *                                                                    return false;
+     *                             </pre>
+     *
+     * @param serverMethod         This method will invoke at server side with an
+     *                             argument {@code wffBMObject}. The
+     *                             {@code wffBMObject} is the representational
+     *                             JavaScript object returned by
+     *                             {@code jsFilterFunctionBody}.
+     * @param jsFilterFunctionBody The body part of a JavaScript function (without
+     *                             function declaration). It can return a JavaScript
+     *                             object so that it will be available at server
+     *                             side in {@code serverMethod} as
+     *                             {@code wffBMObject} parameter. There are implicit
+     *                             objects {@code event} and {@code source} in the
+     *                             scope.<br>
+     *                             Eg:-
+     *
+     *                             <pre>
+     *                                                                    var bName = source.name;
+     *                                                                    return {buttonName: bName, author:'wff', dateOfYear: 2014};
+     *                             </pre>
+     *
+     * @param jsPostFunctionBody   The body part of a JavaScript function (without
+     *                             function declaration). The {@code wffBMObject}
+     *                             returned by {@code serverMethod} will be
+     *                             available as an implicit object {@code jsObject}
+     *                             in the scope. There are common implicit objects
+     *                             {@code event} and {@code source} in the scope.
+     * @since 12.0.0-beta.2
+     */
+    public OnClick(final boolean preventDefault, final String jsPreFunctionBody, final ServerMethod serverMethod,
+            final String jsFilterFunctionBody, final String jsPostFunctionBody) {
+        setServerMethod(preventDefault, jsPreFunctionBody, serverMethod, jsFilterFunctionBody, jsPostFunctionBody,
+                null);
+    }
+
+    /**
+     * @param preventDefault       true to call event.preventDefault(); on event
+     *
+     *
+     * @param serverMethod         This method will invoke at server side with an
+     *                             argument {@code wffBMObject}. The
+     *                             {@code wffBMObject} is the representational
+     *                             JavaScript object returned by
+     *                             {@code jsFilterFunctionBody}.
+     * @param jsFilterFunctionBody The body part of a JavaScript function (without
+     *                             function declaration). It can return a JavaScript
+     *                             object so that it will be available at server
+     *                             side in {@code serverMethod} as
+     *                             {@code wffBMObject} parameter. There are implicit
+     *                             objects {@code event} and {@code source} in the
+     *                             scope.<br>
+     *                             Eg:-
+     *
+     *                             <pre>
+     *                                                                    var bName = source.name;
+     *                                                                    return {buttonName: bName, author:'wff', dateOfYear: 2014};
+     *                             </pre>
+     *
+     * @param jsPostFunctionBody   The body part of a JavaScript function (without
+     *                             function declaration). The {@code wffBMObject}
+     *                             returned by {@code serverMethod} will be
+     *                             available as an implicit object {@code jsObject}
+     *                             in the scope. There are common implicit objects
+     *                             {@code event} and {@code source} in the scope.
+     * @since 12.0.0-beta.2
+     */
+    public OnClick(final boolean preventDefault, final ServerMethod serverMethod, final String jsFilterFunctionBody,
+            final String jsPostFunctionBody) {
+        setServerMethod(preventDefault, null, serverMethod, jsFilterFunctionBody, jsPostFunctionBody, null);
+    }
+
+    /**
+     * @param preventDefault true to call event.preventDefault(); on event
+     * @param serverMethod   This method will invoke at server side with an argument
+     *                       {@code wffBMObject}. The {@code wffBMObject} is the
+     *                       representational JavaScript object returned by
+     *                       {@code jsFilterFunctionBody}.
+     * @param serverSideData this data will be available in the Event object of
+     *                       ServerMethod.invoke method.
+     * @since 12.0.0-beta.2
+     */
+    public OnClick(final boolean preventDefault, final ServerMethod serverMethod, final Object serverSideData) {
+        setServerMethod(preventDefault, null, serverMethod, null, null, serverSideData);
+    }
+
+    /**
+     * @param preventDefault       true to call event.preventDefault(); on event
+     * @param jsPreFunctionBody    the body part JavaScript function (without
+     *                             function declaration). It must return true/false.
+     *                             This function will invoke at client side before
+     *                             {@code serverMethod}. If the jsPrefunction
+     *                             returns true then only {@code serverMethod}
+     *                             method will invoke (if it is implemented). It has
+     *                             implicit objects like {@code event} and
+     *                             {@code source} which gives the reference of the
+     *                             current tag. <br>
+     *                             Eg:-
+     *
+     *                             <pre>
+     *                                                                    if (source.type == 'button') {
+     *                                                                       return true;
+     *                                                                    }
+     *                                                                    return false;
+     *                             </pre>
+     *
+     * @param serverMethod         This method will invoke at server side with an
+     *                             argument {@code wffBMObject}. The
+     *                             {@code wffBMObject} is the representational
+     *                             JavaScript object returned by
+     *                             {@code jsFilterFunctionBody}.
+     * @param jsFilterFunctionBody The body part of a JavaScript function (without
+     *                             function declaration). It can return a JavaScript
+     *                             object so that it will be available at server
+     *                             side in {@code serverMethod} as
+     *                             {@code wffBMObject} parameter. There are implicit
+     *                             objects {@code event} and {@code source} in the
+     *                             scope.<br>
+     *                             Eg:-
+     *
+     *                             <pre>
+     *                                                                    var bName = source.name;
+     *                                                                    return {buttonName: bName, author:'wff', dateOfYear: 2014};
+     *                             </pre>
+     *
+     * @param jsPostFunctionBody   The body part of a JavaScript function (without
+     *                             function declaration). The {@code wffBMObject}
+     *                             returned by {@code serverMethod} will be
+     *                             available as an implicit object {@code jsObject}
+     *                             in the scope. There are common implicit objects
+     *                             {@code event} and {@code source} in the scope.
+     * @param serverSideData       this data will be available in the Event object
+     *                             of ServerMethod.invoke method.
+     * @since 12.0.0-beta.2
+     */
+    public OnClick(final boolean preventDefault, final String jsPreFunctionBody, final ServerMethod serverMethod,
             final String jsFilterFunctionBody, final String jsPostFunctionBody, final Object serverSideData) {
-        setServerMethod(jsPreFunctionBody, serverMethod, jsFilterFunctionBody, jsPostFunctionBody,
+        setServerMethod(preventDefault, jsPreFunctionBody, serverMethod, jsFilterFunctionBody, jsPostFunctionBody,
                 serverSideData);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.webfirmframework.wffweb.tag.html.attribute.event.
+     * AbstractEventAttribute#getPreventDefault()
+     */
+    @Override
+    public boolean isPreventDefault() {
+        return super.isPreventDefault();
+    }
+
+    /**
+     * true to call event.preventDefault(); on event, it will prevent form
+     * submission to action url if it is added on form. It will set only if there is
+     * {@code ServerMethod}.
+     *
+     * @param preventDefault true to call event.preventDefault(); on event otherwise
+     *                       false.
+     * @since 12.0.0-beta.2
+     */
+    @Override
+    public void setPreventDefault(final boolean preventDefault) {
+        super.setPreventDefault(preventDefault);
     }
 
     /**
