bdd476ca79feb93b4219e27c1640f3830ee2922b
Web Firm Framework <webfirm.framework@gmail.com>
2023-04-23 13:08:45 +0530

    To orderly execute test case to prevent failure

--- wffweb/src/test/java/com/webfirmframework/wffweb/tag/html/images/ImgTest.java
+++ wffweb/src/test/java/com/webfirmframework/wffweb/tag/html/images/ImgTest.java
@@ -17,18 +17,21 @@ package com.webfirmframework.wffweb.tag.html.images;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.OrderWith;
 import org.junit.runner.manipulation.Alphanumeric;
+import org.junit.runners.MethodSorters;
 
 import com.webfirmframework.wffweb.tag.html.AbstractHtml.TagType;
 
 @OrderWith(Alphanumeric.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ImgTest {
 
 
     @Test
-    public void testImg1() {
+    public void atestImg1() {
 
         Img img = new Img(null);
         assertEquals("<img>", img.toHtmlString());
