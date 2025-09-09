fbc0cc0fa1da643f4ee33145ce0cb8b3f91d360e
Web Firm Framework <webfirm.framework@gmail.com>
2025-09-09 17:57:14 +0530

    Commented out some unwanted code.
      -- It's a minor optimization.

--- wffweb/src/main/resources/com/webfirmframework/wffweb/internal/server/page/js/wffClientEvents.js
+++ wffweb/src/main/resources/com/webfirmframework/wffweb/internal/server/page/js/wffClientEvents.js
@@ -163,58 +163,59 @@ document.addEventListener("DOMContentLoaded",
 				});
 			}
 
-			MutationObserver = window.MutationObserver
-					|| window.WebKitMutationObserver;
-
-			var attrObserver = new MutationObserver(function(mutations,
-					observer) {
-				// fired when a mutation occurs
-				console.log('MutationObserver', mutations, observer);
-				var attrNameValues = {};
-
-				for (var i = 0; i < mutations.length; i++) {
-					var mutation = mutations[i];
-					attrNameValues[mutation.attributeName] = mutation.target
-							.getAttribute(mutation.attributeName);
-				}
-
-				console.log('attrNameValues', attrNameValues);
-
-			});
-
-			// define what element should be observed by the observer
-			// and what types of mutations trigger the callback
-			attrObserver.observe(document, {
-				// childList: true,
-				// characterData: false,
-				// should be true otherwise will not invoke function(mutations,
-				// observer)
-				subtree : true,
-				attributes : true
-			});
-
-			var tagObserver = new MutationObserver(
-					function(mutations, observer) {
-						// fired when a mutation occurs
-						console.log('tagObserver MutationObserver', mutations,
-								observer);
-						for (var i = 0; i < mutations.length; i++) {
-							var mutation = mutations[i];
-							var addedNodes = mutation.addedNodes;
-							for (var j = 0; j < addedNodes.length; j++) {
-								var addedNode = addedNodes[j];
-								console.log('addedNode', addedNode);
-							}
-						}
-
-					});
-
-			tagObserver.observe(document, {
-				childList : true,
-				// characterData: false,
-				// should be true otherwise will not invoke function(mutations,
-				// observer)
-				subtree : true
-			});
+// kept for future reference
+//			MutationObserver = window.MutationObserver
+//					|| window.WebKitMutationObserver;
+//
+//			var attrObserver = new MutationObserver(function(mutations,
+//					observer) {
+//				// fired when a mutation occurs
+//				console.log('MutationObserver', mutations, observer);
+//				var attrNameValues = {};
+//
+//				for (var i = 0; i < mutations.length; i++) {
+//					var mutation = mutations[i];
+//					attrNameValues[mutation.attributeName] = mutation.target
+//							.getAttribute(mutation.attributeName);
+//				}
+//
+//				console.log('attrNameValues', attrNameValues);
+//
+//			});
+//
+//			// define what element should be observed by the observer
+//			// and what types of mutations trigger the callback
+//			attrObserver.observe(document, {
+//				// childList: true,
+//				// characterData: false,
+//				// should be true otherwise will not invoke function(mutations,
+//				// observer)
+//				subtree : true,
+//				attributes : true
+//			});
+//
+//			var tagObserver = new MutationObserver(
+//					function(mutations, observer) {
+//						// fired when a mutation occurs
+//						console.log('tagObserver MutationObserver', mutations,
+//								observer);
+//						for (var i = 0; i < mutations.length; i++) {
+//							var mutation = mutations[i];
+//							var addedNodes = mutation.addedNodes;
+//							for (var j = 0; j < addedNodes.length; j++) {
+//								var addedNode = addedNodes[j];
+//								console.log('addedNode', addedNode);
+//							}
+//						}
+//
+//					});
+//
+//			tagObserver.observe(document, {
+//				childList : true,
+//				// characterData: false,
+//				// should be true otherwise will not invoke function(mutations,
+//				// observer)
+//				subtree : true
+//			});
 
 		});
